be6f417684fec9ced8abcf9ddb4c6148ec5bca53
OgakiP <pauloogaki.principal@gmail.com>
2022-04-21 15:52:23 -0300

    feat: add routes for login, home and register page and wrap app.js in index.js with browserRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import GlobalStyle from "./Styles/global";
+import Routes from "./Routes";
 
 const App = () => {
   return (
     <div>
       <GlobalStyle />
+      <Routes/>
     </div>
   );
 };
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,10 +1,21 @@
 import { Switch, Route } from "react-router-dom";
+import Register from "../Pages/Register";
+import Login from "../Pages/Login";
+import Home from "../Pages/Home";
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route exact path="/"></Route>
-    </Switch>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/register">
+          <Register />
+        </Route>
+      </Switch>
   );
 };
 export default Routes;
