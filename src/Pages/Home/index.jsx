78335c51e28fc6d68527c384c05710139da179d7
OgakiP <pauloogaki.principal@gmail.com>
2022-04-21 22:53:29 -0300

    feat: create structure for dashboard page and the request for get user

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,38 @@
-import * as S from "./style"
+import * as S from "./style";
+import { useState, useEffect, useId } from "react";
+import api from "../../Services/api";
+import Logo from "../../Assets/Logo.svg";
+import Button from "../../Components/Button";
 
 const Home = () => {
+  const [userId] = useState(JSON.parse(localStorage.getItem("userId")));
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    api
+      .get(`/users/${userId}`)
+      .then((response) => setUser(response.data))
+      .catch((error) => console.log(error));
+  }, [userId]);
+
   return (
-  <S.Container>
-    <S.HeaderContainer>
+    <S.Container>
+      <S.HeaderContainer>
+        <S.HeaderItems>
+          <img src={Logo} alt="Kenzie Hub Logo" />
+          <Button width="56px" color="var(--grey-3)">
+            Sair
+          </Button>
+        </S.HeaderItems>
+      </S.HeaderContainer>
+      <S.UserContainer>
+        <S.UserItems>
+          <h1>Olá, {user.name}</h1>
+          <span>{user.course_module}</span>
+        </S.UserItems>
+      </S.UserContainer>
       
-    </S.HeaderContainer>
-  </S.Container>
+    </S.Container>
   );
 };
 export default Home;
--- src/Pages/Home/style.js
+++ src/Pages/Home/style.js
@@ -3,10 +3,56 @@ import styled from "styled-components"
 export const Container = styled.div`
 width: 100vw;
 min-height: 100vh;
-background-color: var(--grey-3);
+background-color: var(--grey-4);
 `
 
 export const HeaderContainer = styled.div`
+width: 100%;
+height: 72px;
+padding: 0px 10px 0px 10px;
+box-sizing: border-box;
+display: flex;
+align-items: center;
+justify-content: center;
+`
 
+export const HeaderItems = styled.div`
+width: 90%;
+max-width: 768px;
+display: flex;
+justify-content: space-between;
+`
 
+export const UserContainer = styled.div`
+width: 100%;
+height: 118px;
+display: flex;
+justify-content: center;
+align-items: center;
+border-bottom: 1px solid var(--grey-3);
+border-top: 1px solid var(--grey-3);
+`
+
+export const UserItems = styled.div`
+width: 90%;
+max-width: 768px;
+display: flex;
+flex-direction: column;
+gap: 20px;
+
+@media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+}
+
+h1 {
+    font-size: 18px;
+    color: var(--grey-0);
+}
+
+span {
+    font-size: 12px;
+    font-weight: 600;
+    color: var(--grey-1);
+}
 `
